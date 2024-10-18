3de4f7367082bdaf0ca2f1d34d5f0a9a062bfa09
HmzhDubh <meezo.2015@hotmail.com>
2024-10-18 13:34:02 +0300

    add bmi calc and categorize

--- health_states.py
+++ health_states.py
@@ -1,22 +1,62 @@
+import base
 
 class Health_states():
 
-    def __init__(self, weight: float, height: float, bmi: float, measurements: dict):
+    def __init__(self):
+
+        self.health_states = []
+        self.fileName = 'health_states.json'
+        self.weight = 0
+        self.height = 0
+        self.measure_date = ""
+        self.bmi = 0
+
+    def add_health_states(self, weight, height, date):
 
         self.weight = weight
         self.height = height
-        self.bmi = bmi
-        self.measurements = measurements
+        self.bmi = self.calc_bmi()
+        self.date = date
+        self.health_states = []
+        new_measurement = {
+            "height": height,
+            "weight": weight,
+            "bmi": self.calc_bmi(),
+            "date": self.date
 
-    def add_weight(self, weight):
-        self.weight = weight
-    def calc_bmi(self):
-        bmi = self.weight * self.height ** 2
-        pass
+        }
+        self.health_states.append(new_measurement)
+        base.save_to_file(self.fileName, self.health_states)
+        print("data saved successfully ")
+
+    def calc_bmi(self, weight, height):
+        bmi = weight / (height ** 2)
+        return bmi
     def get_bmi(self):
-        return self.bmi
-        pass
-    def get_measurements(self):
-        pass
-    def track_prgress(self):
+        bmi = self.weight / (self.height ** 2)
+        self.bmi = bmi
+        return bmi
+    def get_health_states(self):
+        self.health_states = base.load_from_file(self.fileName)
+        for i in self.health_states:
+            print(i)
+    def bmi_categorization(self, bmi):
+        if bmi < 18.5:
+            print("You are classified as Underweight, which may increase the RISK of developing health problems")
+        elif 18.5 < bmi < 24.9:
+            print("Nice, you are at the normal range of weight, we hope you keep it that way to avoid health problems")
+        elif 25.0 < bmi < 29.9:
+            print("You are classified as OVERWEIGHT, which may INCREASE the RISK of developing health problems")
+            print("watch your weight, and maintain a healthier life style and workout")
+        elif 30 < bmi < 34.9:
+            print("You are classified as OBESE CLASS I, which may have HIGH RISK of developing health problems")
+            print("Be careful, your at risk please consider visiting the Nutritionist and take regular health checks")
+        elif 35 < bmi < 39.9:
+            print("You are classified as OBESE CLASS II, which may have VERY HIGH RISK of developing health problems")
+            print("Be careful, your at risk please consider visiting the Nutritionist and take regular health checks")
+        elif bmi > 40:
+            print("You are classified as OBESE CLASS III, which may have EXTREMELY HIGH RISK of developing health problems")
+            print("Be careful, you're at risk please consider visiting the Nutritionist and take regular health checks")
+
+    def track_progress(self):
         pass
--- main.py
+++ main.py
@@ -2,7 +2,82 @@ from colorama import Fore, Style
 
 from workouts import Workout
 from nutrition import Meal
-# Nutritions Menu
+from health_states import Health_states
+
+
+# health states menu
+def healthStatesMenu():
+
+    print(Fore.CYAN + "=" * 40)
+    print("    (Workouts) Tracker")
+    print("=" * 40 + Style.RESET_ALL)
+    print("Please choose an option from the menu below:")
+    print(Fore.GREEN + "1. Add Health States") # done
+    print("2. Get Curring BMI") # done
+    print("3. Display all Health States") # done
+    # print("4. Track your Progress") # todo
+    print("[Q/q]. Return to Main Menu" + Style.RESET_ALL)
+    print("=" * 40)
+
+
+def healthStatesTracking():
+
+    health_states_instance = Health_states()
+
+    while True:
+
+        healthStatesMenu()
+        choice = input("Enter Your Choice: ")
+
+        if choice == "1":
+            weight = float(input("Enter Your Weight: "))
+            height = float(input("Enter Your Height: "))
+            date = input("Enter measurement date: ")
+
+            health_states_instance.add_health_states(weight, height, date)
+            input(" >>> Press any Key to continue <<< ")
+
+        elif choice == "2":
+            if health_states_instance.get_bmi() == 0:
+
+                # enter data
+                weight = float(input("Enter your weight in (kg ie. 65): "))
+                height = float(input("Enter your Height in (meters ie. 1.66): "))
+
+                #calc bmi
+                bmi = health_states_instance.calc_bmi(weight, height)
+                cat = health_states_instance.bmi_categorization(bmi)
+                print(f"Your BMI is {bmi} which is categorized as {cat}")
+
+                # ask the user to save it or not
+                saveOrnot = input("Do you want to add this data to your file [y/n] ? ")
+                if saveOrnot.lower() == 'y':
+                    date = input("please Enter date: ")
+                    health_states_instance.add_health_states(weight, height, date)
+
+                # get bmi
+                # bmi = health_states_instance.calc_bmi()
+                # cat = health_states_instance.bmi_categorization(bmi)
+                # print(f"Your BMI is {bmi} which is categorized as {cat}")
+            else:
+                bmi = health_states_instance.get_bmi()
+                cat = health_states_instance.bmi_categorization(bmi)
+                print(f"Your BMI is {bmi} which is categorized as {cat}")
+
+            input(" >>> Press any Key to continue <<< ")
+
+        elif choice == "3":
+            health_states_instance.get_health_states()
+            input(" >>> Press any Key to continue <<< ")
+
+        elif choice.lower() == "q":
+            print("returning to main menu <<<")
+            break
+        else:
+            print("Invalid choice! Please enter a number between 1 and 4 or q to return to main menu.")
+
+
+# Nutrition's Menu
 def nutritionsTrackingMenu():
     print(Fore.CYAN + "=" * 40)
     print("    (Workouts) Tracker")
@@ -32,6 +107,7 @@ def nutritionTracking():
             water_intake = input("Enter your water intake in liters: ")
             meal_date = input("Enter the date of the meal or skip for today's date: ")
             nutrition_instance.add_meal(meal_name, meal_calories, meal_macronutrients, water_intake, meal_date)
+            input(" >>> Press any Key to continue <<< ")
 
         elif choice == '2':
 
@@ -40,24 +116,34 @@ def nutritionTracking():
             m_attribute = input("Enter the meal attribute that you want to update [name, calories, macronutrients, water, date]: ")
             m_new_value = input("Enter the new attribute's value to update: ")
             nutrition_instance.update_meal(m_num, m_attribute, m_new_value)
+            input(" >>> Press any Key to continue <<< ")
 
         elif choice == '3':
             # print all meals function
             nutrition_instance.get_meals()
+            input(" >>> Press any Key to continue <<< ")
 
         elif choice == '4':
+
             isCutomized = input("Do you want a customized Suggestion [y/n] otherwise Random? ")
             if isCutomized.lower() == "y":
+
                 diet = input("Enter diet type [vegetarian, vegan, keto, paleo] or enter None: ")
                 maxCalories = int(input("Enter maximum calories wanted in the meal: "))
                 numOfSuggestions = int(input("Enter the Number of suggested meals you want: "))
                 type = input("Enter the meal type [main course, side dish, salad, snack, soup, appetizer, dessert, drink, beverage]: ")
                 nutrition_instance.suggest_meals(diet, maxCalories, numOfSuggestions, type)
+
+            else:
+                nutrition_instance.suggest_meals()
+            input(" >>> Press any Key to continue <<< ")
+
         elif choice.lower() == 'q':
             print("Returning to the main menu <<<")
             break
+
         else:
-            print("Invalid choice! Please enter a number between 1 and 8.")
+            print("Invalid choice! Please enter a number between 1 and 4 or q to return to main menu.")
 
 # workout Menu
 def workoutTrackingMenu():
@@ -100,6 +186,7 @@ def workoutTracking():
             w_attribute = input("Enter the workout attribute that you want to update [type, duration, intensity, calories_burned, date, goals]: ")
             w_new_value = input("Enter the new attribute value to update: ")
             workout_instance.update_workout(w_num, w_attribute, w_new_value)
+            input(" >>> Press any Key to continue <<< ")
         elif choice == '3':
 
             workout_instance.get_workouts()
@@ -112,12 +199,13 @@ def workoutTracking():
             duration_goal = input("Enter the duration goal: ")
             calories_burned_goal = input("Enter the calories to burn goal: ")
             workout_instance.set_goals(w_num, [duration_goal, calories_burned_goal])
+            input(" >>> Press any Key to continue <<< ")
 
         elif choice.lower() == 'q':
             print("Returning to the main menu >>>")
             break
         else:
-            print("Invalid choice! Please enter a number between 1 and 8.")
+            print("Invalid choice! Please enter a number between 1 and 4 or q to return to main menu.")
 
 # Main menu
 def main_menu():
@@ -125,13 +213,11 @@ def main_menu():
     print("    CLI Health and Fitness Tracker")
     print("=" * 40 + Style.RESET_ALL)
     print("Please choose an option from the menu below:")
-    print(Fore.YELLOW + "1. Track Workouts")
-    print("2. Meal and Calorie Tracking")
-    print("3. View Daily/Weekly Health Stats")
-    print("4. Goal Setting")
-    print("5. Track Weight and Body Measurements")
-    print("6. Set Reminders")
-    print("7. Generate Reports")
+    print(Fore.YELLOW + "1. Track Workouts")  # done
+    print("2. Meal and Calorie Tracking")  # done
+    print("3. Track health states and body mass index")  # done
+    print("5. Set Reminders")
+    print("6. Generate Reports")
     print(Fore.RED + "[q/Q]. Exit" + Style.RESET_ALL)
     print("=" * 40)
 
@@ -146,22 +232,21 @@ def main():
             input(" >>> Press any Key to continue <<< ")
 
         elif choice == '2':
-            # todo Call the meal logging function
             nutritionTracking()
             input(" >>> Press any Key to continue <<< ")
         elif choice == '3':
-            #Todo Call the health stats viewing function
-            pass
+            healthStatesTracking()
+
         elif choice == '4':
             #Todo Call the goal setting function
             pass
-        elif choice == '5':
+        elif choice == '4':
             #Todo Call the weight and measurements tracking function
             pass
-        elif choice == '6':
+        elif choice == '5':
             #Todo Call the reminders management function
             pass
-        elif choice == '7':
+        elif choice == '6':
             #Todo Call the report generation function
             pass
         elif choice.lower() == 'q':
