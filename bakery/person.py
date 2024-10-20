e6a526569b5a66ec18be77029cbf35cf7198109a
Ghaliah <ghalia.banouna@gmail.com>
2024-10-20 12:16:36 +0300

    Added separate menu of choices for users and employees

--- bakery/person.py
+++ bakery/person.py
@@ -4,11 +4,13 @@ import pickle, json
 
 class Person:
 
-    def __init__(self, name: str, age: int, gender: str, phone: str) -> None:
+    def __init__(self, name: str, age: int, gender: str, phone: str, password: str) -> None:
         self.__name = name
         self.__age = age 
         self.__gender = gender
         self.__phone = phone
+        self.__password = password
+        self.__role = ''
 
     def setName(self, name: str):
         '''
@@ -34,6 +36,12 @@ class Person:
         '''    
         self.__phone = phone
 
+    def setPassword(self, password: str):
+        '''
+        Setter for gender attribute
+        '''    
+        self.__password = password    
+
     def getName(self) -> str:
         '''
         Getter for name attribute
@@ -58,12 +66,25 @@ class Person:
         '''
         return self.__phone
     
+    def getPassword(self) -> str:
+        '''
+        Getter for password attribute
+        '''
+        return self.__password
+    
+    def getRole(self):
+        '''
+        Getter for role attribute
+        '''
+        return self.__role
+    
     
 class Customer(Person):
 
-    def __init__(self, name: str, age: int, gender: str, phone: str):
-        super().__init__(name, age, gender, phone)
+    def __init__(self, name: str, age: int, gender: str, phone: str, password: str):
+        super().__init__(name, age, gender, phone, password)
         self.__orderHistory: list[OrderedProduct] = []
+        self.__role = "customer"
 
     def setOrderHistory(self, orderHistory: list[OrderedProduct]):
         '''
@@ -77,6 +98,12 @@ class Customer(Person):
         '''
         return self.__orderHistory
     
+    def getRole(self):
+        '''
+        Getter for role attribute
+        '''
+        return self.__role
+    
     def saveToFile(self):
         '''
         Call this when you want to store your customer data in a pickle file
@@ -88,21 +115,28 @@ class Customer(Person):
 
 class Employee(Person):
 
-    def __init__(self, name: str, age: int, gender: str, phone: str):
-        super().__init__(name, age, gender, phone)
+    def __init__(self, name: str, age: int, gender: str, phone: str, password: str):
+        super().__init__(name, age, gender, phone, password)
+        self.__role = "employee"
+
+    def getRole(self):
+        '''
+        Getter for role attribute
+        '''
+        return self.__role
 
     def addProduct(self, prodName: str, qty: int, price: float):
         '''
         This method adds a product to a json file containing all products
         '''
         #Use the method below to get the menu from the json file
-        menu: dict = self.loadFromJSON()
+        menu: dict = self.__loadFromJSON()
 
         menu[prodName]= {
             'qty': qty,
             'price': price
         }
-        with open("../bakeryData/menu.json", "w", encoding="utf-8") as file:
+        with open("bakeryData/menu.json", "w", encoding="utf-8") as file:
             #Store the modified menu in a json file 
             json.dump(menu, file, indent = 4)
     
@@ -110,13 +144,13 @@ class Employee(Person):
         '''
         This method removes a product from the menu then saves the modified menu to the json file
         '''
-        menu: dict = self.loadFromJSON()
+        menu: dict = self.__loadFromJSON()
         
         if menu:
             #Check if the product is on the menu
             if prodName in menu:
                 del menu[prodName]
-                with open("../bakeryData/menu.json", "w", encoding="utf-8") as file:
+                with open("bakeryData/menu.json", "w", encoding="utf-8") as file:
                     #Store the modified menu back to the json file 
                     json.dump(menu, file, indent = 4)
                 return f"Product '{prodName}' was deleted successfully."
@@ -125,18 +159,39 @@ class Employee(Person):
         else: 
             return "Your menu is empty."
 
+    def updateProductName(self, prodName: str, newName):
+        '''
+        This method updates a product's name then saves the modified menu to the json file
+        '''
+        menu: dict = self.__loadFromJSON()
+        
+        if menu:
+            #Check that the product is on the menu
+            if prodName in menu:
+                #Update the price of the product
+                menu[newName] = menu[prodName]
+                del menu[prodName]
+                with open("bakeryData/menu.json", "w", encoding="utf-8") as file:
+                    #Store the modified menu back to the json file 
+                    json.dump(menu, file, indent = 4)
+                return f"Name of '{prodName}' was updated successfully."
+            else:
+                return f"Product '{prodName}' isn't on the menu."
+        else: 
+            return "Your menu is empty."
+
     def updateProductQty(self, prodName: str, qty: int):
         '''
         This method updates a product's quantity then saves the modified menu to the json file
         '''
-        menu: dict = self.loadFromJSON()
+        menu: dict = self.__loadFromJSON()
         
         if menu:
             #Check that the product is on the menu
             if prodName in menu:
                 #Modify the quantity of the product
                 menu[prodName]["qty"] = qty
-                with open("../bakeryData/menu.json", "w", encoding="utf-8") as file:
+                with open("bakeryData/menu.json", "w", encoding="utf-8") as file:
                     #Store the modified menu back to the json file 
                     json.dump(menu, file, indent = 4)
                 return f"Quantity of '{prodName}' was updated successfully."
@@ -149,14 +204,14 @@ class Employee(Person):
         '''
         This method updates a product's price then saves the modified menu to the json file
         '''
-        menu: dict = self.loadFromJSON()
+        menu: dict = self.__loadFromJSON()
         
         if menu:
             #Check that the product is on the menu
             if prodName in menu:
                 #Update the price of the product
                 menu[prodName]["price"] = price
-                with open("../bakeryData/menu.json", "w", encoding="utf-8") as file:
+                with open("bakeryData/menu.json", "w", encoding="utf-8") as file:
                     #Store the modified menu back to the json file 
                     json.dump(menu, file, indent = 4)
                 return f"Price of '{prodName}' was updated successfully."
@@ -164,29 +219,56 @@ class Employee(Person):
                 return f"Product '{prodName}' isn't on the menu."
         else: 
             return "Your menu is empty."
+        
+    def updateProduct(self, prodName: str, choice: str) -> str:
+        '''
+        This method takes product name and choice as arguments and updates the product based on the choice entered
+        If the choice was 1 -> update product name
+        If the choice was 2 -> update product quantity
+        If the choice was 3 -> update product price
+        '''
+        while choice != '4':
+            try:
+                if choice == '1':
+                    newName: str = input("Enter the new name: ")
+                    return self.updateProductName(prodName, newName)
+                elif choice == '2':
+                    newQty: int = int(input("Enter the new quantity: "))
+                    if not isinstance(newQty, int):
+                        raise ValueError("Please enter qunatity in numbers only.")
+                    return self.updateProductQty(prodName, newQty)
+                elif choice == '3':
+                    newPrice: float = float(input("Enter the new price: "))
+                    if not isinstance(newPrice, float):
+                        raise ValueError("Please enter price in numbers only.")
+                    return self.updateProductPrice(prodName, newPrice)
+                else:
+                    print("Invalid choice, try again..")
+            except ValueError as e:
+                print(e)
 
     def listAllProducts(self):
         '''
         This method retrieves the list of all products from json file and returns a formatted string containing the menu
         '''
         menuFormatted: str = ""
-        menu: dict = self.loadFromJSON()
+        menu: dict = self.__loadFromJSON()
 
         if menu:
             #Format and store the menu 
             for prod in menu:
-                menuFormatted += f"⟡ {prod} Quantity: {menu[prod]['qty']} Price per piece: {menu[prod]['qty']}SR.\n"
+                menuFormatted += f"⟡ {prod}.\tQuantity: {menu[prod]['qty']}\tPrice per piece: {menu[prod]['qty']}SR.\n"
             return menuFormatted
         else: 
             return "Your menu is empty."
         
-    def loadFromJSON(self) -> dict: 
+    def __loadFromJSON(self) -> dict: 
         '''
         Call this method whenever you want to load from a json file and make neccessary checks
         '''
         menu: dict = {}
         try:
-            with open("../bakeryData/menu.json", "r", encoding="utf-8") as file:
+            with open("bakeryData/menu.json", "r", encoding="utf-8") as file:
                 #Get the information from the json file by using .load() function
                 menu = json.load(file)
         except FileNotFoundError:
--- main.py
+++ main.py
@@ -1,7 +1,136 @@
-#Main program
+import pickle
+from bakery.person import Person, Employee, Customer
 
+users: list[Person] = []
+
+def loadUsers():
+    '''
+    This function gets a list of users from a file
+    '''
+    global users
+    try:
+        with open("bakeryData/users.pkl", "rb") as file:
+            #Read the list of bank accounts from a pickle file and store it in bankAccounts list
+            users = list(pickle.load(file))
+    except FileNotFoundError:
+        print("File doesn't exist.")
+    except Exception as e:
+        print(e)
+
+def saveUsers(user: Person):
+    '''
+    This function saves users to a pickle file
+    '''
+    loadUsers()
+
+    users.append(user)
+    with open("bakeryData/users.pkl", "wb") as file:
+            #Store list of bank accounts in a pickle file
+            pickle.dump(users, file)
+
+
+def checkUser(phone: str, password: str):
+    '''
+    This function checks if a user is registered in the system
+    '''
+    loadUsers()
+    for user in users:
+        if user.getPhone() == phone and user.getPassword() == password:
+            print(f"Welcome back {user.getName()}")
+            return user
+    print("User isn't registered.")
+     
+def employeeMenu(employee: Employee):
+    '''
+    This function displays a menu of available actions for the employee
+    '''
+    print("-" * 30)
+    while True:
+        print("What would you like to do?")
+        print("1. Add a product.\n2. Remove a product.\n3. Update a product.\n4. View the menu.\n5. Exit.")
+        choice: str = input("Your choice: ")
+        if choice == '1':
+            print("------------------Adding Product------------------")
+            prodName: str = input("Product name: ")
+            while True:
+                try:
+                    qty: int = int(input("Quantity: "))
+                    if not isinstance(qty, int):
+                        raise ValueError("Please enter qunatity in numbers only.")
+                    price: float = float(input("Price: "))
+                    if not isinstance(qty, int):
+                        raise ValueError("Please enter price in numbers only.")
+                except ValueError as e:
+                    print(e)
+                else:
+                    employee.addProduct(prodName, qty, price)
+                    print(f"{prodName} was added successfully.")
+                    break
+            input("")
+
+        elif choice == '2':
+            print("------------------Removing Product------------------")
+            prodName: str = input("Product name: ")
+            removeMsg: str = employee.removeProduct(prodName)
+            print(removeMsg)
+            input("")
+            
+        elif choice == '3':
+            print("------------------Updating a Product------------------")
+            prodName: str = input("Product name: ")
+            print("1. Update product name.\n2. Update product quantity\n3. Update product price.\n4. Back to main menu.")
+            updateChoice: str = input("Your choice: ")
+            updateMsg: str = employee.updateProduct(prodName, updateChoice)
+            print(updateMsg)
+            input("")
+
+        elif choice == '4':
+            print("------------------Stellar Bakery Menu------------------")
+            menu: str = employee.listAllProducts()
+            print(menu)
+            input("")
+
+        elif choice == '5':
+            print("Exiting program..")
+            break
+        
+        else:
+            print("Invalid choice, try again..")
+
+def customerMenu(user: Customer):
+    '''
+    This function displays a menu of available actions for the customer
+    '''
+    print("This is Customer")
+
+#Main program
 def main():
     '''
     Function that contains all main operations
     '''
-    pass
+    print("-------Welcome to Stellar Bakery-------")
+
+    while True:
+        print("Enter '1' to Login and '2' to Register")
+        choice: str = input("Your choice: ")
+        if choice == '1':
+            
+            usrPhone: str = input("Enter your phone number: ")
+            password: str = input("Enter your password: ")
+            
+            user: Person = checkUser(usrPhone, password)
+            if user.getRole() == "employee":
+                employeeMenu(user)
+                break
+            elif user.getRole() == "customer":
+                customerMenu(user)
+                break
+        elif choice == '2':
+            
+            break
+        else:
+            print("Invalid choice, try again..", end=" ")
+            input("")
+
+#Executing main
+main()
