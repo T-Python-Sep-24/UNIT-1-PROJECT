e006ed743c78124f131e721ed5552e4a1355ba1d
osama_fahad <osamaf.net@gmail.com>
2024-10-21 10:20:23 +0300

    done from shopping file

--- Customers.py
+++ Customers.py
@@ -3,12 +3,20 @@ from ShoppingCart import ShoppingCart
 class Customer:
     def __init__(self, name:str, username:str, password:str):
         self.name = name
-        self.username = username
-        self.password = password
+        self.__username = username
+        self.__password = password
         self.cart = ShoppingCart()
         
+    def get_password(self):
+        return self.__password
+    
+    def get_username(self):
+        return self.__username
+    
     def Reset_ShoppingCart(self):
         self.cart = ShoppingCart()
         
+    def checkout(self):
+        pass
         
     
--- ShoppingCart.py
+++ ShoppingCart.py
@@ -4,22 +4,32 @@ class ShoppingCart:
     def __init__(self):
         self.cart = {}
         
-    def add_to_cart(self, product:Products, quntity:int):
-        self.cart[product.product_id] = [product, quntity]
+    def add_to_cart(self, product:Products, quantity:int):
+        if quantity <= product.quantity:
+            self.cart[product.product_id] = [product, quantity]
+            product.quantity -= quantity
+            return True
+        
+        return False
         
     def display(self):
-        for i, product in enumerate(self.cart, start=1):
-            print(f'{i}. Name: {product[0].name}, Price: {product[0].price}, Quantity: {product[1]}')
+        if self.is_empty():
+            return False
+        else:
+            for i, product in enumerate(self.cart, start=1):
+                print(f'{i}. Name: {product[0].name}, Price: {product[0].price}, Quantity: {product[1]}')
+                
+            return True
     
-    def delete_from_cart(self, product:Products, updatedQuntity:int, all=False):
+    def delete_from_cart(self, product:Products, unWantedQuantity:int, all=False):
         if product:
-            if all:
+            newQuantity = self.cart[product.product_id][1] - unWantedQuantity
+            if all or newQuantity <= 0:
                 del self.cart[product.product_id]
             else:
-                self.cart[product.product_id][1] = updatedQuntity
+                self.cart[product.product_id][1] -= unWantedQuantity
             return True
         
         return False
     
-    def checkout(self):
-        pass
+    
